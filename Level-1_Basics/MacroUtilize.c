98aa7123ece2c23fecdaee053e40091d34cce4dd
RoshmiCodes <roshmi.dasgupta.2006@gmail.com>
2025-10-30 19:59:48 +0530

    Add MacroUtilize.c - a program to make use of MACRO in C

---
+++ Level-1_Basics/MacroUtilize.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define P printf
+int main()
+{
+    P("\nThis program implements the declaration and definition of MACRO.");
+    P("\nThis is another application of the preprocessor directive in C.");
+    P("\nThe required function is given a name by an identifier.");
+    P("\nThe identifier then can be used everywhere in the program to access the function.");
+    P("\nNo memory space is allocated.");
+    P("\nThe control identifies the function before the compilation process.");
+    P("\nMACRO name should always be in caps.");
+}
